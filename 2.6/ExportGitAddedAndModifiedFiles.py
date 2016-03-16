4dab18017ad3d3ebd94ec18f94cbdd86cd1fa9a2
panxiaoyong <panxiaoyong@zw310.com>
2016-03-16 19:35:24 +0800

    export files changed or added between two commit for git

--- 2.6/ExportGitAddedAndModifiedFiles.py
+++ 2.6/ExportGitAddedAndModifiedFiles.py
@@ -1,19 +1,58 @@
+#coding=utf-8
 import subprocess
+import os
+import shutil
 
 gitRepositoryPath = "E:/j6/fresh/resource/"
+
+outputPath = "f:/git输出/"
 
 startCommitSign = "931d78f7ed1372aa5c598b20f35180f19089b04c"
 
 endCommitSign = "0cd9b397c63c6999932418008bfe68d19b6df8d5"
 
-#[(A|C|D|M|R|T|U|X|B)…
+def copyFilesToDestination(log):
+    files = str(log).split("\n")
+
+    count = 0
+
+    for fileUrl in files:
+        desFolderUrl = ""
+
+        indexOfSlash = fileUrl.rfind("/")
+
+        if indexOfSlash != -1:
+            desFolderUrl = outputPath + fileUrl[0:fileUrl.rfind("/")]
+        else:
+            desFolderUrl = outputPath
+
+        realStartPath = unicode(gitRepositoryPath + fileUrl, "utf8")
+
+        realDesPath = unicode(desFolderUrl, "utf8")
+
+        if not os.path.exists(realDesPath):
+            os.makedirs(realDesPath)
+
+        print(unicode(str(gitRepositoryPath + fileUrl), "utf8"))
+
+        if fileUrl == "":
+            print("file url is empty")
+        else:
+            if os.path.exists(realStartPath):#判断下文件是否存在，不存在会报错
+                # shutil.copyfile(realStartPath, realFileDesPath)
+                shutil.copy(realStartPath, realDesPath)
+
+        count = count + 1
+
+    print("total files:" + str(count))
+
+#[(A|C|D|M|R|T|U|X|B)
 def gitStatus(startCommit, endCommit, repoDir):
     cmd = 'git diff --name-only --diff-filter=ACMR %s %s %s'%(endCommit,startCommit,repoDir)
     pipe = subprocess.Popen(cmd, shell=True, cwd=repoDir,stdout = subprocess.PIPE,stderr = subprocess.PIPE )
     (out, error) = pipe.communicate()
-    print out,error
-    #k = out.split("\n")
-    #print(k[0])
+    #print out,error
+    copyFilesToDestination(out)
     pipe.wait()
     return
 
