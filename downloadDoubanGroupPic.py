a13d05bb66797c32c1c417b4a5e011c81ff5cc55
xf5464 <xf5464@gmail.com>
2016-10-18 17:21:46 +0800

    下载小组首页贴里的图片完善

--- downloadDoubanGroupPic.py
+++ downloadDoubanGroupPic.py
@@ -6,13 +6,15 @@ import threading
 import time
 
 
-localPath = "e:/doubanDownLoad/group2/"
+localPath = "./group/"
 
 groupName = "haixiuzu"
 
-maxPage = 1000
+maxPage = 1
 
 eachPageSize = 25
+
+interval_time = 20 #20s刷一次首页
 
 proxyList = ["39.83.13.122:8080", "113.68.87.45:8090", "171.39.234.222:80", "182.90.39.60:80"]
 
@@ -24,7 +26,9 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/2010
 
 def downloadGroupTopicPictures(groupName):
 
-    changeProxy()
+    #changeProxy()
+
+    fetchCount = 0
 
     global maxPage
 
@@ -39,31 +43,59 @@ def downloadGroupTopicPictures(groupName):
     if not os.path.exists(parentFolder):
         os.makedirs(parentFolder)
 
-    for i in range(0,maxPage):
-        downloadOnePageTopPics(i*eachPageSize, parentFolder)
+    while True:
+        #定时去取第一页
+        print(_get_time_string() + " fetchCount:" + str(fetchCount))
 
-        count = count + 1
+        for i in range(0,maxPage):
+            #找到第一个已经存在的时候，break
+            downloadOnePageTopPics(groupName, i*eachPageSize, parentFolder)
 
-        if count >= 5:
-            changeProxy()
-            print("sleep 30s")
-            time.sleep(30)
-            count = 0
+        fetchCount = fetchCount + 1
+
+        time.sleep(interval_time)
 
     print("download finished")
 
-def downloadOnePageTopPics(startIndex, parentFolder):
+def downloadOnePageTopPics(groupName, startIndex, parentFolder):
 
-    url = "https://www.douban.com/group/haixiuzu/discussion?start=" + str(startIndex)
+    url = "https://www.douban.com/group/" + groupName + "/discussion?start=" + str(startIndex)
+
+    print(_get_time_string() + " check url")
 
     topicUrlSet = getTopicList(url)
 
     print("has elements:" + str(len(topicUrlSet)))
 
     for topicUrl in topicUrlSet:
-        downloadOneTopicPictures(topicUrl, parentFolder)
+
+        _topic_info = _get_topic_info(topicUrl)
+
+        date = _topic_info[0]
+
+        author = _topic_info[1]
+
+        title = _topic_info[2] \
+
+        if author == "":
+            continue
+
+        _sub_folder_name =  title + "-by-" + author
+
+        _full_folder_name = parentFolder + "/" + date + "/" + _translate_file_Name(_sub_folder_name)
+
+        if os.path.exists(_full_folder_name):
+            continue
+        else:
+            os.makedirs(_full_folder_name)
+
+        print(_get_time_string() + ":" + " load" + title + " by" + author)
+
+        downloadOneTopicPictures(topicUrl, _full_folder_name)
 
     print("finish page " + str(startIndex + 1))
+
+    return True
 
 def getTopicList(url):
 
@@ -86,11 +118,66 @@ def downloadOneTopicPictures(url, parentFolder):
     for url in urls:
         downloadFile(url, parentFolder)
 
+def _get_topic_info(url):
+
+    decodeHtml = getWebPageHtml(url)
+
+    dateInfoPattern = re.compile(r"\<span class=\"color-green\">.*\<\/span\>")
+
+    dateInfoData = dateInfoPattern.findall(decodeHtml)
+
+    if len(dateInfoData) == 0:
+        return ("", "", "")
+
+    index1 = dateInfoData[0].find(">")
+
+    index2 = dateInfoData[0].find(" ", index1)
+
+    date = dateInfoData[0][index1 + 1:index2]
+
+    titlePattern = re.compile(r"h1>.*</h1>", re.S)
+
+    tileInfo = titlePattern.findall(decodeHtml)
+
+    title = ""
+
+    author = ""
+
+    if len(tileInfo) >= 1:
+         titlePattern2 = re.compile(r"\n.*\n")
+
+         z = titlePattern2.findall(tileInfo[0])
+
+         z1 = z[0].replace("\n", "")
+
+         z1 = z1.strip()
+
+         title = z1
+
+    authorPattern1 = re.compile(r"<span class=\"from\">.*</a>")
+
+    authorInfo = authorPattern1.findall(decodeHtml)
+
+    authorName = ""
+
+    if len(authorInfo) >= 1:
+        index1 = authorInfo[0].find(">")
+
+        index2 = authorInfo[0].find(">", index1 + 1)
+
+        index3 = authorInfo[0].find("<", index2)
+
+        authorName = authorInfo[0][index2 + 1:index3]
+
+    ret = (date, authorName,title)
+
+    return ret
+
 def getPicUrlInOneTopic(url):
 
     decodeHtml = getWebPageHtml(url)
 
-    topicPattern = re.compile(r"https://img1.doubanio.com/view/group_topic/large/public/p[0-9]+.jpg")
+    topicPattern = re.compile(r"https://img[0-9]*.doubanio.com/view/group_topic/large/public/p[0-9]+.jpg")
 
     photoUrls = topicPattern.findall(decodeHtml)
 
@@ -106,12 +193,16 @@ def downloadFile(url, folder):
 
     endIndex = url.rfind(".")
 
-    imageName = url[startIndex:]
+    imageName = _translate_file_Name(url[startIndex:])
 
-    data = urllib.request.urlopen(url).read()
+    try:
+        data = urllib.request.urlopen(url).read()
+    except urllib.error.URLError as e:
+        return
+
    # data = urllib.FancyURLopener(proxies).open(url).read()
 
-    f = open(folder + imageName,"wb")  
+    f = open(folder  + "/" + imageName,"wb")
     f.write(data)  
     f.close()
 
@@ -120,18 +211,11 @@ def getWebPageHtml(url):
   #  proxy_handler = urllib.request.ProxyHandler({'http':"39.83.13.122:8080"})
    # opener = urllib.request.build_opener(proxy_handler)
     #urllib.request.install_opener(opener)
-    page = urllib.request.urlopen(url)
-    html = page.read()
-    #req = urllib.request.Request(url=url, headers=headers)
-   # html = req.urlopen(req).read()
 
-    #global opener
-
-    #proxy_handler = urllib.request.ProxyHandler({'http': proxyList[userProxyIndex]})
-   # opener = urllib.request.build_opener(proxy_handler)
-    #urllib.request.install_opener(opener)
-
-    #html = opener.open(url).read()
+    try:
+        html = urllib.request.urlopen(url).read()
+    except urllib.error.URLError as e:
+        return ""
 
     decodeHtml = html.decode("utf-8")
 
@@ -163,7 +247,13 @@ def changeProxy():
 def getTimeStr():
      return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 
-changeProxy()
+def _translate_file_Name(_file_name):
+    r1 = re.compile("\\\|\*|\<|\>|\||\?|\/")
+    return re.sub(r1, "a",  _file_name)
+
+def _get_time_string():
+    return time.strftime('%Y-%m-%d %H:%M:%S')
+#changeProxy()
 
 downloadGroupTopicPictures(groupName)
 
